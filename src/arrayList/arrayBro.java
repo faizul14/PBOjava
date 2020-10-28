bc8a3c7d4fc1c71063c953a33e6f597c4399c447
faizul14 <fpcyber61@gmail.com>
2020-10-28 09:20:09 +0800

    di tambahkan isi

--- src/arrayList/arrayBro.java
+++ src/arrayList/arrayBro.java
@@ -1,14 +1,37 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package arrayList;
+
+import java.util.ArrayList;
 
 /**
  *
  * @author cyber61
  */
 public class arrayBro {
-    
+
+    public static void main(String[] args) {
+        ArrayList list = new ArrayList();
+        
+        //untuk penambahan data seperti ini
+        list.add("andi");
+        list.add("budi");
+        list.add("doni");
+        
+        //untuk mengetahui panjang array list
+        System.out.println(list.size());
+        
+        //untuk menampilkan data di arraty list
+        System.out.println(list.get(1));
+        
+        //untuk menampilkan semua isi araay
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+        
+        int i = 0;
+        while (i < list.size()) {            
+            System.out.println(list.get(i));
+            
+            i++;
+        }
+    }
 }
